ef2a1cc01695b60b14145ef213f572b75680542a
Morena Tirana <63554265+MorenaTirana@users.noreply.github.com>
2022-05-20 20:35:58 +0200

    Add files via upload

---
+++ project.js
@@ -0,0 +1,23 @@
+let projects = [
+    {
+        name: "project 1",
+        tags: "#javascript, #fullstack, #ui/ux, #backend",
+        image: "project (1).png",
+    },
+    {
+        name: "project 2",
+        tags: "#javascript, #fullstack",
+        image: "project (2).png",
+    },
+    {
+        name: "project 3",
+        tags: "#ui/ux",
+        image: "project (3).png",
+    },
+    {
+        name: "project 4",
+        tags: "#ui/ux, #backend",
+        image: "project (4).png",
+    },
+
+]
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+
+dotenv.config();
+let initialPath = path.join(__dirname, "public");
+let app = express();
+
+app.use(express.static(initialPath));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initialPath, "index.html"));
+})
+
+app.post('/mail', (req, res) => {
+    const { firstname, lastname, email, msg } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail', 
+        auth: {
+            user: process.env.EMAIL, 
+            pass: process.env.PASSWORD
+        }
+    })
+
+    const mailOptions = {
+        from: 'anna@gmail.com',
+        to: 'tiranamorena@gmail.com',
+        subject: 'Postfolio',
+        text: `First name: ${firstname}, \nLast name: ${lastname}, \nEmail: ${email}, 
+        \nMessage: ${msg}`
+    }
+
+    transporter.sendMail(mailOptions, (err, result) => {
+        if(err){
+            console.log(err);
+            res.json('ops! Some error occurred. Please try again.')
+        } else{
+            res.json('thanks for e-mailing me. I will revert to you within 2 working days');
+        }
+    })
+})
+
+app.listen('3001', () => {
+    console.log('listening.....');
+})
